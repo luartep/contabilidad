8bbcd53cf8f27e86b6f0350cf92dbeeefc0d5012
luartep <luartep@gmail.com>
2026-07-09 17:43:52 -0400

    Delete lib directory

--- lib/db.ts
+++
@@ -1,5 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-// Requiere la variable de entorno DATABASE_URL configurada en Vercel
-// (Neon la entrega al crear el proyecto / integración).
-export const sql = neon(process.env.DATABASE_URL!);
